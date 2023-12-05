d9d72d6b7c59068f37434e031154720475d41dce
LordAlister <khalef.sofiane91@gmail.com>
2023-12-05 13:49:10 +0100

    final edit

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -60,7 +60,7 @@ useEffect(()=>{
                     <Currency value={totalPrice} />
                 </div>
             </div>
-            <Button onClick={onCheckout} className="w-full mt-6">
+            <Button disabled={items.length === 0} onClick={onCheckout} className="w-full mt-6">
                 Checkout
             </Button>
         </div>
